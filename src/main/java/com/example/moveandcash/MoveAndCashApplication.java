3825bb84517f4e0c663983af47494d0e1f383cad
M1nseoPark <kjms0820@gmail.com>
2023-07-28 15:29:21 +0900

    init

---
+++ src/main/java/com/example/moveandcash/MoveAndCashApplication.java
@@ -0,0 +1,13 @@
+package com.example.moveandcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveAndCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveAndCashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moveandcash/MoveAndCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moveandcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveAndCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
